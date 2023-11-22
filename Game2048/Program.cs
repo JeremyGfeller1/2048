8c42762ffaeb66807ca7c58cad6fb74754fa6e17
JeremyGfeller1 <149375894+JeremyGfeller1@users.noreply.github.com>
2023-11-22 10:15:03 +0100

    Detect arrows keys

--- Game2048/Program.cs
+++ Game2048/Program.cs
@@ -6,7 +6,36 @@ namespace Game2048
     {
         static void Main()
         {
-            
+            bool continueGame = true;
+
+            while (continueGame)
+            {
+                ConsoleKeyInfo keyInfo = Console.ReadKey(true);
+                ConsoleKey key = keyInfo.Key;
+
+                switch (key)
+                {
+                    case ConsoleKey.LeftArrow:
+                        Console.WriteLine("Vous avez appuyé sur la flèche de gauche");
+                        break;
+                    case ConsoleKey.RightArrow:
+                        Console.WriteLine("Vous avez appuyé sur la flèche de droite");
+                        break;
+                    case ConsoleKey.UpArrow:
+                        Console.WriteLine("Vous avez appuyé sur la flèche du haut");
+                        break;
+                    case ConsoleKey.DownArrow:
+                        Console.WriteLine("Vous avez appuyé sur la flèche du bas");
+                        break;
+                    case ConsoleKey.C:
+                        Console.WriteLine("Fin du programme vous avez appuyé sur la touche C");
+                        continueGame = false;
+                        break;
+                    default:
+                        Console.WriteLine("Appuyez sur les touches directionnelles ou la touche C pour quitter le jeu.");
+                        break;
+                }
+            }
         }
     }
 }
